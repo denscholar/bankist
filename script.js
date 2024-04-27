3a34ae6f54e26e0ef1eae665dde2ab0804e2a8c4
Dennis Akagha <d.akagha20@gmail.com>
2024-04-27 10:20:27 +0100

    new project repository set up

---
+++ script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const account1 = {
+  accountName: "Dennis Akagha",
+  movement: [200, 300, 120, -100, -300],
+  interestRate: 0.7,
+  pin: 1123,
+};
+const account2 = {
+  accountName: "Janet John",
+  movement: [20, -300, 1200, -100, 90],
+  interestRate: 0.2,
+  pin: 1125,
+};
+const account3 = {
+  accountName: "John Doe",
+  movement: [50, 100, -120, 10, 300],
+  interestRate: 1.5,
+  pin: 1124,
+};
+const account4 = {
+  accountName: "Peter Smith",
+  movement: [10, 200, 20, -100, -30],
+  interestRate: 1.2,
+  pin: 2222,
+};
